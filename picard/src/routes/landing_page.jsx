6ba409c0e4587e8ab4e8d7b9a6da85471fb62e14
Jordan Dennison <jpd0021@mix.wvu.edu>
2024-09-04 11:03:54 -0400

    width correction

--- picard/src/routes/landing_page.jsx
+++ picard/src/routes/landing_page.jsx
@@ -1,21 +1,27 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import '../App.css';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "../App.css";
 
 const LandingPage = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleEnterClick = () => {
-        navigate('/login');
-    };
+  const handleEnterClick = () => {
+    navigate("/login");
+  };
 
-    return (
-        <div className="min-h-screen flex flex-col justify-between items-center bg-[#001D3D]">
-            <h1 className="text-[72px] font-bold mt-8" style={{ fontFamily: 'Tourney, sans-serif', color: '#FFD60A' }}>
-                THE PICARD
-            </h1>
-            <pre className="text-white text-center" style={{ fontFamily: 'monospace', color: '#FFD60A' }}>
-{`___________________          _-_
+  return (
+    <div className="w-screen min-h-screen flex flex-col justify-between items-center bg-[#001D3D]">
+      <h1
+        className="text-[72px] font-bold mt-8"
+        style={{ fontFamily: "Tourney, sans-serif", color: "#FFD60A" }}
+      >
+        THE PICARD
+      </h1>
+      <pre
+        className="text-white text-center"
+        style={{ fontFamily: "monospace", color: "#FFD60A" }}
+      >
+        {`___________________          _-_
          \\==============_=_/ ____.---'---\`---.____
                     \\_ \\    \\----._________.----/
                \\ \\   /  /    \`-_-'
@@ -23,17 +29,21 @@ const LandingPage = () => {
       /____          ||
           \`--.____,-'
 `}
-            </pre>
-            <div className="mb-8">
-                <button
-                    className="text-[36px] font-bold"
-                    style={{ fontFamily: 'Tourney, sans-serif', color: '#FFD60A', backgroundColor: 'transparent' }}
-                    onClick={handleEnterClick}
-                >
-                    ENTER
-                </button>
-            </div>
-        </div>
-    );
-}
+      </pre>
+      <div className="mb-8">
+        <button
+          className="text-[36px] font-bold"
+          style={{
+            fontFamily: "Tourney, sans-serif",
+            color: "#FFD60A",
+            backgroundColor: "transparent",
+          }}
+          onClick={handleEnterClick}
+        >
+          ENTER
+        </button>
+      </div>
+    </div>
+  );
+};
 export default LandingPage;
