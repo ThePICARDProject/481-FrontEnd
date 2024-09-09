3af738e85961a20ccbbc56244b3882798d94ba34
Jordan Dennison <jpd0021@mix.wvu.edu>
2024-09-09 11:03:50 -0400

    Name Change

--- picard/src/routes/home.jsx
+++ picard/src/routes/home.jsx
@@ -1,32 +1,35 @@
-import React from 'react';
-import ExperimentCard from '../components/experimentcard/experimentcard';
-
+import React from "react";
+import ExperimentCard from "../components/experimentcard/experimentcard";
 
 function Home() {
-    const experiments = [
-        { name: 'Experiment 1', link: 'http://example.com/1', state: 'Completed' },
-        { name: 'Experiment 2', link: 'http://example.com/2', state: 'Completed' },
-        { name: 'Experiment 3', link: 'http://example.com/3', state: 'Completed' },
-        { name: 'Experiment 4', link: 'http://example.com/3', state: 'Completed' },
-        { name: 'Experiment 5', link: 'http://example.com/3', state: 'Completed' },
-        { name: 'Experiment 6', link: 'http://example.com/3', state: 'Completed' },
-        { name: 'Experiment 100', link: 'http://example.com/3', state: 'Completed' },
-    ];
+  const experiments = [
+    { name: "Experiment 1", link: "http://example.com/1", state: "Completed" },
+    { name: "Experiment 2", link: "http://example.com/2", state: "Completed" },
+    { name: "Experiment 3", link: "http://example.com/3", state: "Completed" },
+    { name: "Experiment 4", link: "http://example.com/3", state: "Completed" },
+    { name: "Experiment 5", link: "http://example.com/3", state: "Completed" },
+    { name: "Experiment 6", link: "http://example.com/3", state: "Completed" },
+    {
+      name: "Experiment 100",
+      link: "http://example.com/3",
+      state: "Completed",
+    },
+  ];
 
-    return (
-        <div className="w-screen h-screen flex justify-center items-center overflow-auto ">
-            <div className="w-3/4 h-3/4 max-h-screen border border-gray-300 p-4">
-                {experiments.map((experiment, index) => (
-                    <ExperimentCard 
-                        key={index} 
-                        experimentName={experiment.name} 
-                        link={experiment.link} 
-                        experimentState={experiment.state}
-                    />
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className="w-screen h-screen flex justify-center items-center overflow-auto ">
+      <div className="w-3/4 h-3/4 max-h-screen border border-gray-300 p-4">
+        {experiments.map((experiment, index) => (
+          <ExperimentCard
+            key={index}
+            experimentName={experiment.name}
+            link={experiment.link}
+            experimentState={experiment.state}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
