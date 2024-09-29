6193d48b3d38eb99b5091c33ac7c8e4c745359dd
Ian Rudy <ijrudy@mix.wvu.edu>
2024-09-29 19:13:44 -0400

    Run experiment and upload data buttons beautification

--- picard/src/components/fileuploader/fileuploader.jsx
+++ picard/src/components/fileuploader/fileuploader.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
-import { useState } from 'react';
-import axios from 'axios'
-import Papa from "papaparse"
-import  { useAuth } from "../authprovider/authprovider"
+import Papa from "papaparse";
+import './fileuploader.css';
 const FileUploader = () => {
-
-
     const handleFileChange = (e) => {
-        const file = e.target.files[0]
+        const file = e.target.files[0];
         Papa.parse(file, {
             complete: (res) => {
-                console.log(res.data)
+                console.log(res.data);
             }
-        })
-    }
+        });
+    };
+
     return (
-        <form>
-            <input onChange={handleFileChange} type='file' /> 
-        </form>  
+        <form style={{ margin: 0, padding: 0, width: '100%' }}>
+            <label className="upload-button">
+                Upload Data Set File
+                <input 
+                    type="file" 
+                    onChange={handleFileChange} 
+                    className="hidden-file-input"
+                />
+            </label>
+        </form>
     );
 };
 
