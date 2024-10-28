4c1129e08aab2ef8491246e321de7c2513d2df44
jrhauser <jrh00045@mix.wvu.edu>
2024-10-28 12:47:08 -0400

    removed chunked file uploads for demonstration

--- picard/src/components/fileuploader/fileuploader.jsx
+++ picard/src/components/fileuploader/fileuploader.jsx
@@ -5,29 +5,19 @@ const FileUploader = () => {
     e.preventDefault();
     const file = e.target.files[0];
     const formData = new FormData();
-    const chunkSize = 1024*1024;
-    let start = 0;
-    let total = Math.ceil(file.size / chunkSize);
-    while (start < file.size) {
-      const formData = new FormData()
-      formData.append('file', file.slice(start, start + chunkSize))
-      formData.append("name", file.name);
-      formData.append("description", "test");
-      const res = await axios.post(
-        "http://localhost:5080/api/dataset/upload",
-        formData,
-        {
-          headers: {
-            "ChunkNumber": start + 1,
-            "TotalChunks": total,
-            "Content-Type": "multipart/form-data",
-          },
-        }
-      );
-            
-            start += chunkSize
-
-        }
+    formData.append("file", file);
+    formData.append("name", file.name);
+    formData.append("description", "test");
+    const res = await axios.post(
+      "http://localhost:5080/api/dataset/upload",
+      formData,
+      {
+        withCredentials: true,
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
   };
   return (
     <form>
