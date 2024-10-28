d097797007168f9f940e1d6ff95791898eb267b9
jrhauser <jrh00045@mix.wvu.edu>
2024-10-28 11:15:04 -0400

    testing chunked uploads

--- picard/src/components/fileuploader/fileuploader.jsx
+++ picard/src/components/fileuploader/fileuploader.jsx
@@ -8,18 +8,29 @@ const FileUploader = () => {
     e.preventDefault();
     const file = e.target.files[0];
     const formData = new FormData();
-    formData.append("file", file);
-    formData.append("name", file.name);
-    formData.append("description", "test");
-    const res = await axios.post(
-      "http://localhost:5080/api/dataset/upload",
-      formData,
-      {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      }
-    );
+    const chunkSize = 1024*1024;
+    let start = 0;
+    let total = Math.ceil(file.size / chunkSize);
+    while (start < file.size) {
+      const formData = new FormData()
+      formData.append('file', file.slice(start, start + chunkSize))
+      formData.append("name", file.name);
+      formData.append("description", "test");
+      const res = await axios.post(
+        "http://localhost:5080/api/dataset/upload",
+        formData,
+        {
+          headers: {
+            "ChunkNumber": start,
+            "TotalChunks": total,
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+            
+            start += chunkSize
+
+        }
   };
   return (
     <form>
